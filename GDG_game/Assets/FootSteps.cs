f69c2bfb57481f5b38b8f8430a8ef2c5ba986024
Cocca <alicecocca@gmail.com>
2020-06-03 21:58:41 +0200

    CoseSuono1

--- GDG_game/Assets/FootSteps.cs
+++ GDG_game/Assets/FootSteps.cs
@@ -9,10 +9,15 @@ public class FootSteps : MonoBehaviour
     [SerializeField]  private AudioClip[] clipsJ;
     private AudioSource audioSourceS;
     private AudioSource audioSourceJ;
+    [SerializeField] private AudioClip[] clipsD;
+    private AudioSource audioSourceD;
+
     private void Awake()
     {
 
         audioSourceS = GetComponent<AudioSource>();
+        audioSourceJ = GetComponent<AudioSource>();
+        audioSourceD = GetComponent<AudioSource>();
     }
     private void Step()
     {
@@ -33,5 +38,19 @@ public class FootSteps : MonoBehaviour
     private AudioClip GetRandomClipJ()
     {
         return clipsS[UnityEngine.Random.Range(0, clipsJ.Length)];
+    }
+    
+    private void Death ()
+    {
+        AudioClip clip = GetRandomClipD();
+
+        audioSourceD.PlayOneShot(clip);
+
+
+
+    }
+    private AudioClip GetRandomClipD()
+    {
+        return clipsD[UnityEngine.Random.Range(0, clipsD.Length)];
     }
 }
