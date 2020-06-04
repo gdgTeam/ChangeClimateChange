a949d07d899d1d78fbde23868a651665f3d7d0cb
Fabiana <fabyyy.spa@gmail.com>
2020-06-04 22:22:50 +0200

    Sfondo città con fuocherelli e luci

--- GDG_game/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
+++ GDG_game/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
@@ -9,6 +9,7 @@ namespace UnityStandardAssets.Effects
         private float m_Rnd;
         private bool m_Burning = true;
         private Light m_Light;
+        public int strength;
 
 
         private void Start()
@@ -22,7 +23,7 @@ namespace UnityStandardAssets.Effects
         {
             if (m_Burning)
             {
-                m_Light.intensity = 1000*Mathf.PerlinNoise(m_Rnd + Time.time, m_Rnd + 1 + Time.time*1) ;
+                m_Light.intensity = strength * Mathf.PerlinNoise(m_Rnd + Time.time, m_Rnd + 1 + Time.time*1) ;
                 float x = Mathf.PerlinNoise(m_Rnd + 0 + Time.time*2, m_Rnd + 1 + Time.time*2) - 0.5f;
                 float y = Mathf.PerlinNoise(m_Rnd + 2 + Time.time*2, m_Rnd + 3 + Time.time*2) - 0.5f;
                 float z = Mathf.PerlinNoise(m_Rnd + 4 + Time.time*2, m_Rnd + 5 + Time.time*2) - 0.5f;
