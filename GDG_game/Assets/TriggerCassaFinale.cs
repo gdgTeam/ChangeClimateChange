3a41937e0855d79c19af91a0e5b68f228bf18075
Francesco Calì <francescocali.97@gmail.com>
2020-06-10 19:04:08 +0200

    Puzzle ascensore completo forse spero

---
+++ GDG_game/Assets/TriggerCassaFinale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class TriggerCassaFinale : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Pushable")
+            {
+                Rigidbody rg = other.transform.GetComponent<Rigidbody>();
+                rg.isKinematic = true;
+            }
+        }
+    }
+}
+
