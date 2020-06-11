e21e2ec1de43e10e616005018856ac5b6538dd2a
Fabiana <fabyyy.spa@gmail.com>
2020-06-11 12:59:50 +0200

    pushettino

---
+++ GDG_game/Assets/Scenes/Prove_animazioni/Scripts/StateBase/Fall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/Fall")]
+    public class Fall : StateData
+    {
+
+        public float timeLeft =1.5f; 
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            timeLeft = 1.5f;
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                animator.SetBool("FallToDie", true);
+            }
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            timeLeft = 1.5f;
+        }
+    }
+}
