213507acfdd1be0320c68b7cc60484aff9cd276a
Francesco Calì <francescocali.97@gmail.com>
2020-06-13 18:46:40 +0200

    Inizio camminata robot

---
+++ GDG_game/Assets/Robot/Script/RobotControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace roundbeargames_tutorial
+{
+    public class RobotControl : MonoBehaviour
+    {
+        public bool Moving;
+        public bool MoveLeft;
+        public bool MoveRight;
+        public bool Stopping;
+        public bool Turning;
+
+        private Vector3 left;
+        private Vector3 right;
+
+
+        void Start()
+        {
+            left = new Vector3(0, 180f, 0);
+            right = new Vector3(0, 0, 0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(this.gameObject.transform.rotation.eulerAngles == left)
+            {
+                MoveLeft = true;
+                MoveRight = false;
+            }
+            else if( this.gameObject.transform.rotation.eulerAngles == right)
+            {
+                MoveRight = true;
+                MoveLeft = true;
+            }
+        }
+    }
+}
---
+++ GDG_game/Assets/Robot/Script/StateBase/Robot_moveForward.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/Robot_MoveForward")]
+    public class Robot_moveForward : StateData
+    {
+        private RobotControl control;
+        public float Speed;
+        public AnimationCurve SpeedGraph;
+
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            control = animator.GetComponentInParent<RobotControl>();
+            if (control.MoveRight) 
+                control.transform.Translate(Vector3.forward * Speed * SpeedGraph.Evaluate(stateInfo.normalizedTime) * Time.deltaTime);
+            if(control.MoveLeft)
+                control.transform.Translate(Vector3.forward * Speed * SpeedGraph.Evaluate(stateInfo.normalizedTime) * Time.deltaTime);
+
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            
+        }
+    }
+}
