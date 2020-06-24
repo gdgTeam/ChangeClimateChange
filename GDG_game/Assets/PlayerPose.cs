43ca22d637d9d47097b39bd6d0c90fb150a0c7b6
Pietro <pingi.pie95@gmail.com>
2020-06-24 19:38:18 +0200

    Ok

--- GDG_game/Assets/PlayerPose.cs
+++ GDG_game/Assets/PlayerPose.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Security.AccessControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -51,24 +52,18 @@ namespace roundbeargames_tutorial
                 SceneManager.LoadScene("Scena_foresta", LoadSceneMode.Single);
                 SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
                 CharacterControl control = player.GetComponent<CharacterControl>();
-                player.GetComponent<CharacterControl>().plant = true;
-                player.GetComponent<CharacterControl>().sparaOk = true;
-                control.transform.GetChild(3).gameObject.transform.GetComponent<SkinnedMeshRenderer>().enabled = true;
-                control.transform.GetChild(2).gameObject.transform.GetComponent<SkinnedMeshRenderer>().enabled = true;
-                control.zainoPianta.SetActive(false);
-                control.zainoPianta.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
-                control.RIGID_BODY.isKinematic = false;
-                control.checkPickFalse = true;
-               // control.plant = true;
-                control.protectPlant = true;
-                control.zainoPianta.transform.parent = GameObject.Find("mixamorig: RightHand").transform;
-                control.zainoPianta.transform.position = new Vector3(0.631783f, 1.027834f, 0.3071281f);
+                if (!control.protectPlant)
+                {
+                    control.zainoPianta.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 0.2f);
+                }
+                /*control.zainoPianta.transform.parent = GameObject.Find("mixamorig: RightHand").transform;
+                control.zainoPianta.transform.localPosition = new Vector3(0.631783f, 1.027834f, 0.3071281f);
                 
-                control.zainoPianta.transform.rotation = Quaternion.Euler(-142.972f, -67.46899f, 43.646f);
+                control.zainoPianta.transform.localRotation = Quaternion.Euler(-142.972f, -67.46899f, 43.646f);
                
                 control.zainoPianta.transform.localScale = new Vector3(77.20261f, 77.20261f, 77.20261f);
-
-
+               */
+                
                 player.GetComponent<CharacterControl>().Die = false;
                 
 
--- GDG_game/Assets/Trigger_inizioCitta.cs
+++ GDG_game/Assets/Trigger_inizioCitta.cs
@@ -5,6 +5,9 @@ namespace roundbeargames_tutorial {
     public class Trigger_inizioCitta : MonoBehaviour
     {
         private bool enter;
+        private GameObject RobotFor1;
+        private GameObject RobotFor2;
+        private GameObject RobotCitta;
 
         private void Start()
         {
@@ -18,6 +21,12 @@ namespace roundbeargames_tutorial {
                 enter = true;
                 FindObjectOfType<AudioManager>().StopPlaying("audio_foresta");
                 FindObjectOfType<AudioManager>().Play("audio_citta");
+                RobotFor1 = GameObject.Find("Robot(5)");
+                RobotFor2 = GameObject.Find("Robot(6)");
+                RobotCitta = GameObject.Find("Robot_(1)");
+                RobotFor1.SetActive(false);
+                RobotFor2.SetActive(false);
+                RobotCitta.SetActive(true);
             }
         }
 
